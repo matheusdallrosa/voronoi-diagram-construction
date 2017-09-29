260d28e86a927e7c00f9e04b5806d0f7196bc4ae
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2017-09-29 17:38:34 -0300

    Tests for the entire point class

--- fortune_points/__tests__/geom/point_unittest.cpp
+++ fortune_points/__tests__/geom/point_unittest.cpp
@@ -1,9 +1,29 @@
 #include "gtest/gtest.h"
 #include "fortune_points/geom/point.h"
+#include "fortune_points/geom/floating_point.h"
 
 namespace{
-TEST(PointTest, PointConstructor){
-  Point *p = new Point(2,3);
-  ASSERT_NEAR(p->x, 2, 1.0e-11);
-}
+  TEST(PointTest, PointConstructor){
+    Point *p = new Point(100000000,300000000);
+    ASSERT_NEAR(p->x, 100000000, EPS);
+    ASSERT_NEAR(p->y, 300000000, EPS);
+  }
+
+  TEST(PointTest, PointLessThanPredicate1){
+    Point *p = new Point(100000000,300000000),
+          *q = new Point(100000000,300000000.00001);
+    ASSERT_TRUE(p < q);
+  }
+
+  TEST(PointTest, PointLessThanPredicate2){
+    Point *p = new Point(100000000,300000000.000001),
+          *q = new Point(100000000.000001,300000000.000001);
+    ASSERT_TRUE(p < q);
+  }
+
+  TEST(PointTest, PointEqualsPredicate){
+    Point *p = new Point(300000000.000001,300000000.000001),
+          *q = new Point(300000000.000001,300000000.000001);
+    ASSERT_TRUE(p == q);
+  }
 }
