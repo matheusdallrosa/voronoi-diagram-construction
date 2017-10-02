12564f285a2cd9e92203042c5d19cf171a495835
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2017-10-02 10:51:37 -0300

    Comparing the objects not the memory address

--- fortune_points/__tests__/geom/point_unittest.cpp
+++ fortune_points/__tests__/geom/point_unittest.cpp
@@ -7,23 +7,30 @@ namespace{
     Point *p = new Point(100000000,300000000);
     ASSERT_NEAR(p->x, 100000000, EPS);
     ASSERT_NEAR(p->y, 300000000, EPS);
+    delete p;
   }
 
   TEST(PointTest, PointLessThanPredicate1){
     Point *p = new Point(100000000,300000000),
           *q = new Point(100000000,300000000.00001);
-    ASSERT_TRUE(p < q);
+    ASSERT_TRUE(*p < *q);
+    delete p;
+    delete q;
   }
 
   TEST(PointTest, PointLessThanPredicate2){
     Point *p = new Point(100000000,300000000.000001),
           *q = new Point(100000000.000001,300000000.000001);
-    ASSERT_TRUE(p < q);
+    ASSERT_TRUE(*p < *q);
+    delete p;
+    delete q;
   }
 
   TEST(PointTest, PointEqualsPredicate){
     Point *p = new Point(300000000.000001,300000000.000001),
           *q = new Point(300000000.000001,300000000.000001);
-    ASSERT_TRUE(p == q);
+    ASSERT_TRUE(*p == *q);
+    delete p;
+    delete q;
   }
 }
