70fd4202eefd63eea4de3ea8d1804349d670c1a5
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2017-10-03 23:23:32 -0300

    Header to the file with the algo

---
+++ fortune_points/algorithm/fortune.h
@@ -0,0 +1,8 @@
+#include "fortune_points/diagram/site.h"
+#include "fortune_points/diagram/voronoi.h"
+
+#include <vector>
+
+void fortune(std::vector<Site> &sites,
+              std::vector<Edge> &edges,
+              std::vector<Vertice> &vertices);
