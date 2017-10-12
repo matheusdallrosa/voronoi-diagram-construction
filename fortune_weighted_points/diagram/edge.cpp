b29f735527d54b964f059dccaa6f068fb80fd0ed
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2017-10-12 00:06:28 -0300

    Assign sites who generate an edge]

--- fortune_weighted_points/diagram/edge.cpp
+++ fortune_weighted_points/diagram/edge.cpp
@@ -4,10 +4,12 @@ Edge::Edge(int _id,double x,WSite _p,WSite _q){
   this->id = _id;
   this->is_vertical = true;
   this->vertical_line = x;
+  this->p = _p, this->q = _q;
 }
 
-Edge::Edge(int _id,Hyperbole h,WSite p,WSite q){
+Edge::Edge(int _id,Hyperbole h,WSite _p,WSite _q){
   this->id = _id;
   this->is_vertical = false;
   this->hyperbole = h;
+  this->p = _p, this->q = _q;
 }
