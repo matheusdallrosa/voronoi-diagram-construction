18095072a69b9f400ed8367a9fd687363352f61e
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2017-10-11 23:31:09 -0300

    Output generator for a given set of sites

--- fortune_weighted_points/__tests__/algorithm/test_helper/test_output_generator.cpp
+++ fortune_weighted_points/__tests__/algorithm/test_helper/test_output_generator.cpp
@@ -9,15 +9,20 @@ using namespace std;
 #include <vector>
 #include <set>
 
-#include "fortune_points/algorithm/fortune.h"
-#include "fortune_points/diagram/voronoi.h"
-#include "fortune_points/diagram/site.h"
+#include "fortune_weighted_points/algorithm/fortune.h"
+#include "fortune_weighted_points/diagram/wsite.h"
+#include "fortune_weighted_points/diagram/edge.h"
+#include "fortune_weighted_points/diagram/vertice.h"
 
-vector<Site> sites;
+double W;
+vector<WSite> sites;
 void read_sites(){
-  double x,y;
-  while(scanf("%lf %lf",&x,&y) != EOF)
-    sites.push_back(Site(sites.size(),x,y));
+  W = 0;
+  double x,y,wi;
+  while(scanf("%lf %lf %lf",&x,&y,&wi) != EOF){
+    W = std::max(W,wi);
+    sites.push_back(WSite(sites.size(),x,y,wi));
+  }
 }
 
 vector<Edge> edges;
@@ -25,19 +30,21 @@ vector<Vertice> vertices;
 
 int main(void){
   read_sites();
-  fortune(sites,edges,vertices);
+  fortune(sites,W,edges,vertices);
   printf("Number of edges: %d\n",(int)edges.size());
   for(vector<Edge>::iterator it = edges.begin(); it != edges.end(); it++){
     printf("ID: %d\n",it->id);
-    printf("Sites: %d %d\n",it->s0,it->s1);
-    printf("Bisector: %lfx + %lfy + %lf = 0\n\n",it->l.a,it->l.b,it->l.c);
+    printf("Sites: %d %d\n",it->p.get_id(),it->q.get_id());
+    Hyperbole h = it->hyperbole;
+    printf("Bisector: %lfx^2 + %lfxy + %lfy^2 + %lfx + %lfy + %lf = 0\n\n",
+            h.A,h.B,h.C,h.D,h.E,h.F);
   }
   int cnt = 0;
   printf("Number of vertices: %d\n",(int)vertices.size());
   for(vector<Vertice>::iterator it = vertices.begin(); it != vertices.end(); it++){
     printf("V%d = (%lf, %lf)\n",cnt,it->x,it->y);
     cnt++;
-    printf("Incident edges: %d %d %d\n",it->b0,it->b1,it->b2);
+    printf("Incident edges: %d %d %d\n",it->edges[0],it->edges[1],it->edges[2]);
   }
   return 0;
 }
